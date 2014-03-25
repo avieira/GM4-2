8c640b3af9a7d0e9f80498753837fe031dfcf11b
avieira <alexandre.vieira@insa-rouen.fr>
2014-03-25 16:58:38 +0100

    Ajout Projet freeFem et insertion SQL continué. 	renamed:    BdD/SQL/creationBase -> BdD/SQL/creationBase.sql 	modified:   BdD/SQL/insertBase.sql 	new file:   Projet/freeFem/SV.edp 	new file:   Projet/freeFem/bc_SV.edp 	new file:   Projet/freeFem/macro_SV.edp 	new file:   Projet/freeFem/mesh.edp 	new file:   Projet/freeFem/solve_SV.edp

--- BdD/SQL/creationBase.sql
+++ BdD/SQL/creationBase.sql
@@ -81,3 +81,4 @@ ref TINYTEXT,
 PRIMARY KEY noNews,
 FOREIGN KEY noNews REFERENCES News);
 
+
--- BdD/SQL/insertBase.sql
+++ BdD/SQL/insertBase.sql
@@ -1,6 +1,21 @@
---Insertion dans la Sous-Section
+--Insertion dans la Sous-Section, des Sujets, puis des posts
 INSERT INTO Sous-Section VALUES ("Premiers pas", "Règles", "À lire avant toute inscription !");
+	INSERT INTO Sujet VALUES ("Règles", "Règles du forum");
+		INSERT INTO Post VALUES ("Règles du forum", NULL, 25-03-2014, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porta lacinia eleifend. Vivamus fermentum ultricies ante consectetur rutrum massa nunc.", "Admin");
+
+	INSERT INTO Sujet VALUES ("Règles", "Règles du site");
+		INSERT INTO Post VALUES ("Règles du site", NULL, 25-03-2014, "Suspendisse rhoncus lectus nec tortor porta, ut viverra nisl malesuada. Suspendisse eget euismod dui. Duis id enim id nibh euismod lobortis amet.", "Admin");
+
+
 INSERT INTO Sous-Section VALUES ("Premiers pas", "Présentation", "La politesse veut qu'on se présente après l'inscription, histoire qu'on ne vous considère pas comme un inconnu !");
+	INSERT INTO Sujet VALUES ("Présentation", "Premier nouveau");
+		INSERT INTO Post VALUES ("Premier Nouveau", NULL, 25-03-2014, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porta lacinia eleifend. Vivamus fermentum ultricies ante consectetur rutrum massa nunc.", "Pseudo1");
+		INSERT INTO Post VALUES ("Premier Nouveau", NULL, 25-03-2014, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porta lacinia eleifend. Vivamus fermentum ultricies ante consectetur rutrum massa nunc.", "Pseudo2");
+	INSERT INTO Sujet VALUES ("Présentation", "Deuxième arrivé");
+		INSERT INTO Post VALUES ("Deuxième arrivé", NULL, 26-03-2014, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porta lacinia eleifend. Vivamus fermentum ultricies ante consectetur rutrum massa nunc.", "Pseudo3");
+		INSERT INTO Post VALUES ("Deuxième arrivé", NULL, 27-03-2014, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porta lacinia eleifend. Vivamus fermentum ultricies ante consectetur rutrum massa nunc.", "Pseudo1");
+
+
 
 INSERT INTO Sous-Section VALUES ("Informations", "Annonces globales", "Seuls les administrateurs et modérateurs peuvent poster ici. Cette section sert à communiquer avec le bas-peuple.");
 INSERT INTO Sous-Section VALUES ("Informations", "Problèmes sur le site", "Un problème ou une coquille sur le site ? Venez en parler ici.");
