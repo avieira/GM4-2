f4bddd4fae24deb59973188546fa9f6219535dc8
avieira <alexandre.vieira@insa-rouen.fr>
2014-03-25 12:34:55 +0100

    Début insertion sql

---
+++ BdD/SQL/insertBase.sql
@@ -0,0 +1,14 @@
+--Insertion dans la Sous-Section
+INSERT INTO Sous-Section VALUES ("Premiers pas", "Règles", "À lire avant toute inscription !");
+INSERT INTO Sous-Section VALUES ("Premiers pas", "Présentation", "La politesse veut qu'on se présente après l'inscription, histoire qu'on ne vous considère pas comme un inconnu !");
+
+INSERT INTO Sous-Section VALUES ("Informations", "Annonces globales", "Seuls les administrateurs et modérateurs peuvent poster ici. Cette section sert à communiquer avec le bas-peuple.");
+INSERT INTO Sous-Section VALUES ("Informations", "Problèmes sur le site", "Un problème ou une coquille sur le site ? Venez en parler ici.");
+INSERT INTO Sous-Section VALUES ("Informations", "Problèmes sur le forum", "Un problème ou une coquille sur le forum ? Venez en parler ici.");
+INSERT INTO Sous-Section VALUES ("Informations", "News", "Envie de revenir sur une de nos news ? Vous pouvez en discuter avec tous les membres ici !");
+
+INSERT INTO Sous-Section VALUES ("Discussion", "Loisirs et bons plans", "Un loisir à partager ou un bon plan pour les membres ? C'est ici que ça se passe !");
+INSERT INTO Sous-Section VALUES ("Discussion", "Petits jeux", "Parce qu'on ne doit parler que de sujets sérieux ici, les petits jeux servent à se détendre.");
+INSERT INTO Sous-Section VALUES ("Discussion", "Blabla et boîte à flood", "Si vous ne savez pas où poster, ou que ce n'est pas très sérieux... C'est par ici !");
+
+INSERT INTO Sous-Section VALUES ("Administration", "Page d'admin", "Page reservée aux administrateurs.");
