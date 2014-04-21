dab8592c54fb8275db193e007215225bf98f48d3
avieira <alexandre.vieira@insa-rouen.fr>
2014-04-21 16:26:35 +0200

    Ajout test2 pour cpp et MàJ FreeFem 	modified:   ../../Projet/essaiFreeFem/laplacienHomog.edp 	modified:   ../../Projet/freeFem/SV.edp 	modified:   ../../Projet/freeFem/bc_SV.edp 	modified:   ../../Projet/freeFem/macro_SV.edp 	modified:   ../../Projet/freeFem/solve_SV.edp 	new file:   test2/dialog.cpp 	new file:   test2/dialog.h 	new file:   test2/dialog.ui 	new file:   test2/fenprincipale.cpp 	new file:   test2/fenprincipale.h 	new file:   test2/main.cpp 	new file:   test2/test2.pro 	new file:   test2/test2.pro.user

---
+++ cpp/initQt/test2/fenprincipale.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_FENPRINCIPALE
+#define HEADER_FENPRINCIPALE
+
+#include <QApplication>
+#include<QMainWindow>
+
+class FenPrincipale : public QMainWindow
+{
+    public:
+        FenPrincipale();
+
+    private:
+
+};
+
+#endif
